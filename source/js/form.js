0de45c7b58ea7c47e1842eb7fdf1aed1b887ae15
sergevnikiforov <sergevnikiforov@gmail.com>
2018-07-25 08:24:10 +0300

    Validation

--- source/js/form.js
+++ source/js/form.js
@@ -1,26 +1,46 @@
 (function() {
     var me = {};
-
     var form = document.querySelector('.form-container');
     var closeButton = null;
-
 
     function onClose() {
         me.close();
         closeButton.removeEventListener('click', onClose);
-
     }
 
     me.open = function() {
         form.classList.remove('is-hidden');
         closeButton = document.querySelector('.form__close-button');
         closeButton.addEventListener('click', onClose);
-
     };
-
     me.close = function() {
         form.classList.add('is-hidden');
     };
-
-    window.form = me;
-})();
+    me.isValid = function() {
+        var requiredFields = document.querySelectorAll('[data-valid="required"]');
+        var emailValue = document.querySelector('[data-email]').value;
+        var numberValue = document.querySelector('[data-number]').value;
+        if (!me.isAllCompleted(requiredFields)) {
+            alert('Заполните, пожалуйста, все необходимые поля!');
+            return false;
+        } else if (!VALID.validation.isEmail(emailValue)) {
+            alert('Неверно введенный email!');
+            return false;
+        } else if (!VALID.validation.isNumber(numberValue)) {
+            alert('Некорректно введенный номер!');
+            return false;
+        }
+        return true;
+    };
+    me.isAllCompleted = function(data) {
+        var result = true;
+        for (var i = 0; i < data.length; i++) {
+            if (!VALID.validation.isNotEmpty(data[i].value)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    };
+    VALID.form = me;
+}());
--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,30 @@
 (function() {
     var openFormButton = document.querySelector('.btn-down');
+    var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
     if (openFormButton) {
         openFormButton.addEventListener('click', function() {
-            form.open();
+            VALID.form.open();
         })
     }
-
-})();
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (VALID.form.isValid()) {
+                console.log('Форма заполнена верно!');
+            } else {
+                console.log('Введенные данные не прошли проверку на валидность! Форма заполнена неверно!');
+            }
+        })
+    }
+    if (nav) {
+        nav.addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+            e.preventDefault();
+            VALID.navigation.toggleToActiveLink(target);
+        });
+    }
+}());
